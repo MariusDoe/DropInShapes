d44343d6339ff57c1b6dec003040f21c3e6eb1c5
Marius Dörbandt <github.marius@doerbandt.de>
2020-12-05 16:37:05 +0100

    added main camera centering

--- src/Camera2D.gd
+++ src/Camera2D.gd
@@ -1,7 +1,20 @@
 extends Camera2D
 
+class_name MainViewportCamera
+
 func _ready():
 	make_current()
 
-func _process(delta):
-	zoom = Vector2(2, 2)
+func show_size(show: Vector2):
+	var size = (get_parent() as Viewport).size
+	var adjustedShow = show
+	var showOffset = Vector2(0, 0)
+	if show.x / show.y > size.x / size.y:
+		adjustedShow.y = show.x * size.y / size.x
+		showOffset.y -= (adjustedShow.y - show.y) / 2
+	else:
+		adjustedShow.x = show.y * size.x / size.y
+		showOffset.x -= (adjustedShow.x - show.x) / 2
+	var zoom_xy = adjustedShow.x / size.x
+	zoom = Vector2(zoom_xy, zoom_xy)
+	position = showOffset / 2 + adjustedShow / 2
--- src/GameController.gd
+++ src/GameController.gd
@@ -1,11 +1,14 @@
 extends Control
 
 var main: Node2D
+var camera: MainViewportCamera
 var draw: Draw
 
 func _ready():
 	main = get_node("VBoxContainer/MainViewportContainer/Viewport/Main")
 	draw = get_node("VBoxContainer/HBoxContainer/Draw")
+	camera = get_node("VBoxContainer/MainViewportContainer/Viewport/Camera2D")
+	camera.show_size(Vector2(850, 700))
 
 func _on_Draw_done_drawing():
 	add_drawn_static(Vector2(500, 500))
