e7cfd30a2cdc957146067548279145c9cd9543ba
LinusAndersen <emailanlinus@googlemail.com>
2020-12-06 15:31:20 +0100

    LevelDesign + new Ball

--- src/Level.gd
+++ src/Level.gd
@@ -4,6 +4,6 @@ class_name Level
 
 export var view_rect: Rect2 = Rect2(0, 0, 1, 1)
 
-export var countdown: float = 10
+export var countdown: float = 20
 
-export var deathcountdown: float = 20
+export var deathcountdown: float = 26
